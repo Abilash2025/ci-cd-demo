43bc87da92da617f3d2a8c8f1232f997b91700c9
Abilash2025 <abilash.r2004@gmail.com>
2026-09-15 16:18:50 +0530

    Added HelloControllerTest.java and updated the run command in ci.yml

---
+++ src/test/java/com/devops/demo/controller/HelloControllerTest.java
@@ -0,0 +1,19 @@
+package com.devops.demo.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloControllerTest {
+
+    private final HelloController helloController = new HelloController();
+
+    @Test
+    void hello_shouldReturnExpectedMessage(){
+        String message = helloController.hello();
+
+        assertEquals("Hello from devops-demo", message);
+    }
+
+
+}
